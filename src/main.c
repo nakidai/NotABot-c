8eef06cbaac20860bfefccc49150609d38416afe
Nakidai <alex_platonov1@mail.ru>
2024-03-03 17:50:17 +0300

    add some style

--- src/main.c
+++ src/main.c
@@ -6,17 +6,24 @@
 
 static u64snowflake g_app_id;
 
-void on_ready(struct discord *client, const struct discord_ready *event) {
+void on_ready(
+    struct discord *client,
+    const struct discord_ready *event
+) {
     log_info("NotABot succesfully started!");
     g_app_id = event->application->id;
 }
 
-void on_interaction(struct discord *client, const struct discord_interaction *event) {
+void on_interaction(
+    struct discord *client,
+    const struct discord_interaction *event
+) {
     if (event->type != DISCORD_INTERACTION_APPLICATION_COMMAND)
         return; /* return if interaction isn't a slash command */
 }
 
-int main(void) {
+int main(void)
+{
     ccord_global_init();
     struct discord *client = discord_config_init(CONFIG_PATH);
 
